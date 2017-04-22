a93d5e5fccac34123ddcea672226a045bf2b945d
Sheep <louis.guittard@epitech.eu>
2017-04-22 22:15:23 +0200

    physics in Logic

--- source/Logic.cpp
+++ source/Logic.cpp
@@ -5,18 +5,20 @@
 // Login   <guitta_l@epitech.eu>
 //
 // Started on  Sat Apr 22 10:30:13 2017 Louis Guittard
-// Last update Sat Apr 22 10:32:09 2017 Louis Guittard
+// Last update Sat Apr 22 22:08:25 2017 Louis Guittard
 //
 
 #include <algorithm>
 #include "Logic.hpp"
 
 Logic::Logic()
+: _physics(Vect<2, int>(0, 0), 0.4, 1000)
 {
 }
 
 void Logic::tick(void)
 {
+  this->_physics.makePhysicsOnEntity(this->_entities);
   std::for_each(_entities.begin(), _entities.end(),
 		[](Entity &e) { e.update(); });
 }
