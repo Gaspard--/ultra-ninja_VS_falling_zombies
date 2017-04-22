206d8079fca7abb2f7f5fd70e3ad68cd4117c85f
Sheep <louis.guittard@epitech.eu>
2017-04-22 10:46:42 +0200

    init logic

---
+++ include/Logic.hpp
@@ -0,0 +1,24 @@
+//
+// Logic.hpp for  in /home/sheep/worktree/ultra-ninja_VS_falling_zombies/include/
+//
+// Made by Louis Guittard
+// Login   <guitta_l@epitech.eu>
+//
+// Started on  Sat Apr 22 10:13:21 2017 Louis Guittard
+// Last update Sat Apr 22 10:19:37 2017 Louis Guittard
+//
+
+#ifndef LOGIC_HPP_
+# define LOGIC_HPP_
+
+class Logic
+{
+public:
+    Logic();
+    ~Logic();
+
+public:
+    void tick();
+};
+
+#endif // !LOGIC_HPP_
---
+++ source/Logic.cpp
@@ -0,0 +1,21 @@
+//
+// Logic.cpp for  in /home/sheep/worktree/ultra-ninja_VS_falling_zombies/source/
+//
+// Made by Louis Guittard
+// Login   <guitta_l@epitech.eu>
+//
+// Started on  Sat Apr 22 10:30:13 2017 Louis Guittard
+// Last update Sat Apr 22 10:32:09 2017 Louis Guittard
+//
+
+#include "Logic.hpp"
+
+Logic::Logic()
+{
+
+}
+
+Logic::~Logic()
+{
+
+}
