595c5a2ffded62cb0b9f1b2fbb2f97a748667a3d
melis_m <matteo.melis@epitech.eu>
2017-04-24 14:37:53 +0200

    omg des bullets

--- source/Bullet.cpp
+++ source/Bullet.cpp
@@ -1,11 +1,14 @@
-#include "Bullet.hpp"
+#include "Logic.hpp"
 #include "TextureHandler.hpp"
 
 Bullet::Bullet(Entity &entity)
   : entity(entity), damage(3), lifetime(20)
 {
   entity.fixture.mass = 0;
+  entity.fixture.radius = 0.01;
   entity.renderable.texture = TextureHandler::getInstance().getTexture(TextureHandler::BULLET);
+  entity.renderable.destSize = {entity.fixture.radius * 2.1, entity.fixture.radius * 2.1};
+  entity.renderable.sourceSize = {1, 1};
 }
 
 Bullet::~Bullet()
@@ -14,9 +17,16 @@ Bullet::~Bullet()
 
 void Bullet::update()
 {
-  --lifetime;
-  isUseless = lifetime <= 0;
-  entity.isUseless = isUseless;
+  Player &player = Logic::getInstance().getPlayer();
+  Vect<2, double> vec(-entity.fixture.pos[1], entity.fixture.pos[0]);
+  Vect<2, double> right(entity.fixture.speed * 0.99 - vec.normalized() * (0.0005 * (1.0 + entity.isOnPlanet)) * -1);
+  Vect<2, double> left(entity.fixture.speed * 0.99 + vec.normalized() * (0.0005 * (1.0 + entity.isOnPlanet)) * 1);
+
+  if (((right + entity.fixture.pos) - player.entity.fixture.pos).length2() <
+      ((left + entity.fixture.pos) - player.entity.fixture.pos).length2())
+    entity.fixture.speed = right;
+  else
+    entity.fixture.speed = left;
 }
 
 void Bullet::hit(Enemy &e)
--- source/EnemyShooter.cpp
+++ source/EnemyShooter.cpp
@@ -21,17 +21,19 @@ void EnemyShooter::attack(Player&)
 void EnemyShooter::shoot()
 {
   Logic &logic = Logic::getInstance();
+  Player& player = logic.getPlayer();
   // TODO if (within attack range) -> attack
   // target : player
-  Player& player = logic.getPlayer();
 
+  logic.addBullet(entity.fixture.pos);
+  logic.addBullet(entity.fixture.pos);
   // TODO actual shoot
 }
 
 bool EnemyShooter::update(const Player& player)
 {
   Vect<2, double> vec(-entity.fixture.pos[1], entity.fixture.pos[0]);
-  Vect<2, double> right(entity.fixture.speed * 0.99 + vec.normalized() * (0.0005 * (1.0 + entity.isOnPlanet)) * -1);
+  Vect<2, double> right(entity.fixture.speed * 0.99 - vec.normalized() * (0.0005 * (1.0 + entity.isOnPlanet)) * -1);
   Vect<2, double> left(entity.fixture.speed * 0.99 + vec.normalized() * (0.0005 * (1.0 + entity.isOnPlanet)) * 1);
 
   if (((right + entity.fixture.pos) - player.entity.fixture.pos).length2() <
