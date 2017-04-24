fe6155850584d1bb959342f801c46dd71b545b14
Matthias RIGAUD <matthias.rigaud@epitech.eu>
2017-04-25 01:20:16 +0200

    resolve bug game over && change interface color

--- source/display.cpp
+++ source/display.cpp
@@ -212,8 +212,8 @@ void    Display::displayInterface(void)
               { 0.5 + 0.05 * logic.getOccupedSpace(), 0.5 - 0.05 * logic.getOccupedSpace(), 0.5 - 0.05 * logic.getOccupedSpace()});
   displayText("Current Population",
               256, {0.05f, 0.05f}, {-0.017f * 18, -0.315f}, {sqrt(camera.length2()), 0}, {1.0, 1.0, 1.0});
-  displayText("Score   " + std::to_string(logic.getScore()), 256, {0.1f, 0.1f}, {-1.0 / dim[0], 0.72}, {1, 0}, {1.0, 1.0, 0.5});
-  displayText("Time   " + logic.getTime(), 256, {0.1f, 0.1f}, {-1.0 / dim[0], 0.5}, {1, 0}, {1.0, 0.5, 1.0});
+  displayText("Score   " + std::to_string(logic.getScore()), 256, {0.1f, 0.1f}, {-1.0 / dim[0], 0.72}, {1, 0}, {1.0, 1.0, 1.5});
+  displayText("Time   " + logic.getTime(), 256, {0.1f, 0.1f}, {-1.0 / dim[0], 0.5}, {1, 0}, {1.0, 1.0, 1.0});
   if (logic.getGameOver() == true)
     displayText("Game Over", 256, {0.2f, 0.2f}, {-0.65, 0.4}, {1, 0}, {1.0, 0.25, 0.0});
 }
