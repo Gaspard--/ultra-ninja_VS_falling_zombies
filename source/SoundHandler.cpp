7d0ce9a9d5013ad8ac79e40f387247d29d4b4a27
Jamy Bailly <jamy.bailly@epitech.eu>
2017-04-24 18:01:41 +0200

    [Sound] Add cpp

---
+++ source/SoundHandler.cpp
@@ -0,0 +1,37 @@
+#include "SoundHandler.hpp"
+
+std::unique_ptr<SoundHandler> SoundHandler::_instance(nullptr);
+
+SoundHandler::SoundHandler()
+{
+}
+
+void SoundHandler::initSoundHandler()
+{
+  _instance.reset(new SoundHandler());
+  // _instance->addSound(BOYAUX, "resources/boyaux.bmp");
+  // Already loaded in display.cpp
+  // _instance->addSound(PLANET, "resources/planet.bmp");
+  // _instance->addSound(SWORDRADIUS, "resources/swordRadius.bmp");
+  // _instance->addSound(TEST, "resources/test.bmp");
+}
+
+SoundHandler& SoundHandler::getInstance()
+{
+  return *_instance;
+}
+
+void SoundHandler::destroySoundHandler()
+{
+  _instance.reset(nullptr);
+}
+
+void SoundHandler::addSound(SoundList id, std::string const& path)
+{
+  _instance->_sound[id] = NULL;
+}
+
+sf::Sound SoundHandler::getSound(SoundList id) const
+{
+  return (_instance->_sound[id]);
+}
