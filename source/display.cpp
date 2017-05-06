9384baa09bdf3ac4a40935981018ab3d65ba9cfe
Jamy Bailly <jamy.bailly@epitech.eu>
2017-05-06 22:13:05 +0200

    [macos] fix load opengl

--- source/display.cpp
+++ source/display.cpp
@@ -31,6 +31,10 @@ Display::GlfwContext::GlfwContext()
                        });
   if (!glfwInit())
     throw std::runtime_error("opengl: failed to initialize glfw");
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 }
 
 Display::GlfwContext::~GlfwContext()
